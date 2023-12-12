3a0e12bfd0d5da960a55d2eb8e3a787f3badb956
Your Name <you@example.com>
2023-12-12 10:57:31 -0500

    final

---
+++ script.js
@@ -0,0 +1,45 @@
+import 'https://cdn.jsdelivr.net/npm/chart.js@4.3.0/dist/chart.umd.min.js'
+
+async function wrapper() {
+    let data = [], temp;
+    (await fetch("ZonAnn.Ts+dSST.csv").then(async (response) => {
+        (await response.text()).split("\n").slice(1).forEach((element) => {
+          temp = element.split(",");
+          data.push([temp[0], temp[1], temp[2], temp[3]]);
+        })
+    }))
+
+    new Chart(document.getElementById("chart"), {
+      type: "line",
+      data: {
+        labels: data.map((e) => e[0]),
+        datasets: [{
+          label: `Average Global Temperature`,
+          data: data.map((e) => e[1]),
+          borderColor: 'purple',
+          yAxisID: 'y'
+        }]
+      }, 
+      options: {
+        tooltips: {
+          intersect: true
+        }
+      },
+      scales: {
+        y: {
+          type: 'linear',
+          display: false,
+          position: 'left',
+        },
+        y1: {
+          type: 'linear',
+          display: false,
+          position: 'left',
+        }
+      }
+    })
+  }
+
+  wrapper()
+  
+  localStorage.setItem("t", "p")
